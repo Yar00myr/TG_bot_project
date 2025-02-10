4e33633b81a80337b239a8e01a45b22dc80d75f3
Yar00myr <ivanucaromir@gmail.com>
2025-02-10 15:38:30 +0000

    habit reminder (under development)

--- bot/handlers/users/__init__.py
+++ bot/handlers/users/__init__.py
@@ -4,12 +4,12 @@ from aiogram import Router
 from .start import router as start_commands_router
 from .register import router as register_commands_router
 from .help import router as help_commands_router
-
+from .set_reminder import router as set_reminder_routr
 from .tracking_habit import router as tracking_habits
 
 router = Router(name=__name__)
 
 router.include_routers(start_commands_router, register_commands_router)
-
+router.include_router(set_reminder_routr)
 router.include_router(help_commands_router)
 router.include_router(tracking_habits)
